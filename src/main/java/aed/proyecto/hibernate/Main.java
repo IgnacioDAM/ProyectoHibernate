3740feb0073390e45c61766a31f9b20be36c86a6
Ignacio <joseignacio.perezdelgado@iessantaana.com>
2020-11-09 10:13:00 +0000

    Preparatorias

--- src/main/java/aed/proyecto/hibernate/Main.java
+++ src/main/java/aed/proyecto/hibernate/Main.java
@@ -1,9 +1,9 @@
-package dad.template.javafx;
+package aed.proyecto.hibernate;
 
 public class Main {
 
 	public static void main(String[] args) {
-		TemplateApp.main(args);
+		//.main(args);
 	}
 
 }
